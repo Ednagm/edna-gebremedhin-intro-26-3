8d840553e9d3df2d6dba4da7afba7aad30535692
Ednagm <gebremedhinedna@gmail.com>
2026-09-15 19:27:18 -0600

    fix open api and message editing

--- js/dog.js
+++ js/dog.js
@@ -1,8 +1,29 @@
-console.log("Dog JavaScript is working!");
-
+const breedsButton = document.getElementById("breeds-button");
 const randomDogButton = document.getElementById("random-dog-button");
-const threeDogsButton = document.getElementById("three-dogs-button");
 const dogContainer = document.getElementById("dog-container");
+
+breedsButton.addEventListener("click", function () {
+    fetch("https://dog.ceo/api/breeds/list/all")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            dogContainer.innerHTML = "<h2>Dog Breeds</h2>";
+
+            const breedsList = document.createElement("ul");
+
+            for (const breed in data.message) {
+                const breedItem = document.createElement("li");
+                breedItem.textContent = breed;
+                breedsList.appendChild(breedItem);
+            }
+
+            dogContainer.appendChild(breedsList);
+        })
+        .catch(function () {
+            dogContainer.innerHTML = "<p>Sorry, we couldn't find the dog breeds.</p>";
+        });
+});
 
 randomDogButton.addEventListener("click", function () {
     fetch("https://dog.ceo/api/breeds/image/random")
@@ -20,26 +41,5 @@ randomDogButton.addEventListener("click", function () {
         })
         .catch(function () {
             dogContainer.innerHTML = "<p>Sorry, we couldn't find a dog.</p>";
-        });
-});
-
-threeDogsButton.addEventListener("click", function () {
-    fetch("https://dog.ceo/api/breeds/image/random/3")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            dogContainer.innerHTML = "";
-
-            data.message.forEach(function (dog) {
-                const dogImage = document.createElement("img");
-                dogImage.src = dog;
-                dogImage.alt = "A random dog";
-
-                dogContainer.appendChild(dogImage);
-            });
-        })
-        .catch(function () {
-            dogContainer.innerHTML = "<p>Sorry, we couldn't find the dogs.</p>";
         });
 });
